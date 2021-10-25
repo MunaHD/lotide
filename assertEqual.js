2cdaece8bd423ec406d323f45ea540f08a3d9cbc
Muna Dahir <muna.dahir@hotmail.com>
2021-10-25 16:25:21 -0400

    our own console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual +  " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("happy", "Happy");// I guess its not like the === and it's like the ==
+assertEqual(1, 15);
