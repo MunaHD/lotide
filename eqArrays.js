9b2dc4ef2557c9efec2d5321f959823d9782c9d5
Muna Dahir <muna.dahir@hotmail.com>
2021-10-26 20:59:30 -0400

    ccheck if arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2){
+  if (arr1.every((val, index) => val === arr2[index])){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
