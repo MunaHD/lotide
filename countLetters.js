d90cdf92b234794bf2cba69677d48e58db22b08e
Muna Dahir <muna.dahir@hotmail.com>
2021-10-27 19:49:37 -0400

    made function to make letter count pairs

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+const countLetters = function(string) {
+  //create empty object
+  let results = {};
+  //loop over letters in the string
+  for (const letter of string) {
+    //condition to see if its a letter
+    if ((/[a-zA-Z]/).test(letter)) {
+      // add lett as key and count how many times it appears
+      results[letter] ? results[letter] += 1 : results[letter] = 1;
+    }
+  }
+  //return results
+  return results;
+};
+
+//log the value to the screen
+console.log(countLetters("lighthouse in the house"));
