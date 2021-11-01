ee1aa95b9ae3883e4480305dc46e21f75ad163da
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 14:46:20 -0400

    added required module and some tests

---
+++ test/AssertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("happy", "Happy");// I guess its not like the === and it's like the ==
+assertEqual(1, 15);
