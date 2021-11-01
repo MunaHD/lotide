b9e7c15a5b159a188a4526478d48fc91197dd56c
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 14:09:50 -0400

    added comments to explain the code

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,7 @@
 
 const eqArrays = function (arr1, arr2){
+  // .every checks every value of the array to see if it passes the test which in this case
+  // looking at the 'value' in array1 and check if it is equal to the  'value' of array2 at the same index
   if (arr1.every((val, index) => val === arr2[index])){
     return true;
   } else {
@@ -10,7 +12,7 @@ const eqArrays = function (arr1, arr2){
 
 
 // TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 module.exports = eqArrays;
