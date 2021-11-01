e59a9fa58466bb842b6ff00a8d4cb01c04aaeede
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 09:18:41 -0400

    used JSON to print whole object

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -32,32 +32,33 @@ const eqObjects = function(obj1, obj2) {
 }
 
 const assertObjectsEqual = function (obj1, obj2) {
-  const inspect = require('util').inspect;
+ 
   if (eqObjects(obj1, obj2)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
+    //add JSON.strigify to show the full object (read into JSON more)
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(obj1)} === ${JSON.stringify(obj2)}`);
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`)
+    console.log(`❌❌❌ Assertion Failed: ${JSON.stringify(obj1)} !== ${JSON.stringify(obj2)}`)
   }
 }
 
 
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
 
-assertObjectsEqual(ab, ba);// true
+// assertObjectsEqual(ab, ba);// true
 
-const abc = { a: "1", b: "2", c: "3" };
+// const abc = { a: "1", b: "2", c: "3" };
 
-assertObjectsEqual(ab, abc); // false
-
-
+// assertObjectsEqual(ab, abc); // false
 
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertObjectsEqual(cd, dc);// true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertObjectsEqual(cd, cd2);// false
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertObjectsEqual(cd, dc);// true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertObjectsEqual(cd, cd2);// false
