8ff8c6cf9c183d4ca1a4144fc6d02358297ac79b
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 19:17:35 -0400

    Update findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -18,14 +18,4 @@ const findKeyByValue = function(object, value) {
 
 };
 
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
