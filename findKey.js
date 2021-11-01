e37a870d7965a55e33f78d0180e3a046a927154f
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 19:10:01 -0400

    got rid of extra and added module.exports

--- findKey.js
+++ findKey.js
@@ -1,13 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const findKey = function(object, callback) {
 
   // the function findKey which takes in an object and a callback.
@@ -24,13 +14,8 @@ const findKey = function(object, callback) {
   }
 };
 
-//const findKey = (obj, callback) => Object.keys(obj).find(key => cb(obj[key]));
 
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma"); // => "noma"
+
+module.exports = findkey;
+//personal test 
+//const findKey = (obj, callback) => Object.keys(obj).find(key => cb(obj[key]));
