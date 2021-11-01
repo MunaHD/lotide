965cd0b5310519aa7a2503586a4ee329701a5ca6
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 16:50:32 -0400

    added return values

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
   } else {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   }
 };
 
-module.exports = assertEqual;
+
+  module.exports = assertEqual;
