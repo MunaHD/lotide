9cd6130acbf63fc0cd9e7fbdbc3f8edf5ad5122b
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 09:00:03 -0400

    added module.export to filed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,24 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-const eqArrays = function (arr1, arr2){
-  if (arr1.every((val, index) => val === arr2[index])){
-    return true;
-  } else {
-    return false;
-  }
-}
+const eqArrays = require("./eqArrays");
+
 
 const assertArraysEqual = function (arr1, arr2){
-
+  // uses the eqArray function to see if the arrays are equal
   if (eqArrays(arr1, arr2)) {
+    //if the test passes
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else{
+    //if the test fails
     console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`)
   }
 }
 
 
+
 // TEST CODE
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+
+
+module.exports = assertArraysEqual;
