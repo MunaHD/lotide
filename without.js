8212bf64f13e5fb3c5bc4dd5d70bac58786715dd
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 19:03:58 -0400

    export function

--- without.js
+++ without.js
@@ -54,10 +54,13 @@ const without = function (source, itemRem){
 
 without([1, 2, 3], [2]);
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+module.exports = without;
+
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 
 
