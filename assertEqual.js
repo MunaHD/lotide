ee9b2b2792e1b5d7dc9231a301cd6d9248204c9a
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 09:01:09 -0400

    added the module export

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,9 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("happy", "Happy");// I guess its not like the === and it's like the ==
-assertEqual(1, 15);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("happy", "Happy");// I guess its not like the === and it's like the ==
+// assertEqual(1, 15);
+
+module.exports = assertEqual;
