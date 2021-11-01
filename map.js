9db4c1cad433780fc8b6fadd38f2aa217e0b6e58
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 19:20:26 -0400

    export function

--- map.js
+++ map.js
@@ -10,6 +10,9 @@ const map = function(array, callback) {
  
 };
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
+module.exports = map;
+
+//TEST
+// const results1 = map(words, word => word[0]);
+// console.log(results1);
 
