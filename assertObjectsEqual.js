af41e615f5e1738fefec164d3d3e5dd5908f03ce
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 14:05:54 -0400

    cleaned the code to get rid of the extra code and added require module for eqObjects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,35 +1,9 @@
+const eqObjects = require('./eqObjects')
 
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.every((val, index) => val === arr2[index])) {
-    return true;
-  } else {
-  
-    return false;
-  }
-}
 
-const eqObjects = function(obj1, obj2) {
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false;
-  }
 
-  for (let key in obj1) {
-    if (Object.keys(obj1[key]).length !== Object.keys(obj2[key]).length) {
-      return false;
-    }
-    if (Array.isArray(obj1[key]) &&  Array.isArray(obj2[key])) { 
-      
-      if (eqArrays(obj1[key], obj2[key]) === false) {
-        return false;
-      }
 
-    } else if (obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-  return true;
-}
 
 const assertObjectsEqual = function (obj1, obj2) {
  
@@ -43,22 +17,17 @@ const assertObjectsEqual = function (obj1, obj2) {
 
 
 
+//TEST
+
 // const ab = { a: "1", b: "2" };
 // const ba = { b: "2", a: "1" };
-
-// assertObjectsEqual(ab, ba);// true
-
 // const abc = { a: "1", b: "2", c: "3" };
-
-// assertObjectsEqual(ab, abc); // false
-
-
-
-
-
 // const cd = { c: "1", d: ["2", 3] };
 // const dc = { d: ["2", 3], c: "1" };
-// assertObjectsEqual(cd, dc);// true
-
 // const cd2 = { c: "1", d: ["2", 3, 4] };
+
+
+// assertObjectsEqual(ab, ba);// true
+// assertObjectsEqual(ab, abc); // false
+// assertObjectsEqual(cd, dc);// true
 // assertObjectsEqual(cd, cd2);// false
