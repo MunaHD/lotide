e9db3d4c30c006da1feb21ca4a17c78acc18d90b
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 14:15:30 -0400

    added the assertEqual function as required module

--- eqObjects.js
+++ eqObjects.js
@@ -1,14 +1,18 @@
 const eqArrays = require("./eqArrays");
+const assertEqual = require("./assertEqual");
+
 
 
 const eqObjects = function(obj1, obj2) {
-
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   }
 
   for (let key in obj1) {
-    if (Array.isArray(obj1[key]) &&  Array.isArray(obj2[key]) && obj1[key].length == obj2[key].length) { 
+    if (Object.keys(obj1[key]).length !== Object.keys(obj2[key]).length) {
+      return false;
+    }
+    if (Array.isArray(obj1[key]) &&  Array.isArray(obj2[key])) { 
       
       if (eqArrays(obj1[key], obj2[key]) === false) {
         return false;
@@ -19,32 +23,21 @@ const eqObjects = function(obj1, obj2) {
     }
   }
   return true;
-  
 }
 
 
+
+module.exports = eqObjects;
+
 // Testing
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// const cd2 = { c: "1", d: ["2", 3, 4] };
 
-
-
-
+// assertEqual(eqObjects(cd, cd2), false);
+// assertEqual(eqObjects(cd, dc), true);
 // assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
-
 // assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
 // assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
-
-module.exports = eqObjects;
