746b1838e0ce40317b255a54a1564738954eec30
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 14:17:16 -0400

    Got rid of assetEqual to test code, added module export

--- head.js
+++ head.js
@@ -1,19 +1,10 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual  === expected) {
-   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 
 const head = function (array) {
+
+  /// return (condition) if true then return value at array index 0 else return undefined
   return array[0] ? array[0] : undefined;
 };
 
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
