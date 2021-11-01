5d95d13f3017fbc8b34927e8d49759531a55e064
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 13:49:34 -0400

    got rid of tests

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,11 +6,5 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("happy", "Happy");// I guess its not like the === and it's like the ==
-// assertEqual(1, 15);
 
 module.exports = assertEqual;
