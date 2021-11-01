57e20a02b42fdfb2e8a12e6e4fceb9b156512f87
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 15:33:51 -0400

    edited comments to make more sense, got rid of tests, added exports module

--- middle.js
+++ middle.js
@@ -10,19 +10,17 @@ const middle = function(arr){
   // make variabel to hold index of middle element
 
   let mid = Math.floor(arr.length / 2)
-  let value = [];
-  //ternary operator to return values: 
+  let midValue = [];
+   
+  //ternary operator to return alues: checks the length of array
   //if array is not equal return the value at the middle
   //else return 2 values at the middle
-
   arr.length % 2 !== 0 ?
-    value.push(arr[mid]) : 
-    value.push(arr[mid - 1], arr[mid]);
+    midValue.push(arr[mid]) : 
+    midValue.push(arr[mid - 1], arr[mid]);
 
-  return value
+  return midValue
 
 }
 
-console.log(middle([1, 5, 3, 4, 7])); // 2
-console.log(middle([1, 5, 3, 7])); // 5, 3
-console.log(middle([1, 5])); // []
+module.exports = middle;
