4ab8e02fe52d1e7394897b91f79015d625dc38e7
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 09:29:37 -0400

    added module export

--- eqObjects.js
+++ eqObjects.js
@@ -1,73 +1,50 @@
-// // FUNCTION IMPLEMENTATION
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-const eqArrays = function (arr1, arr2){
-  if (arr1.every((val, index) => val === arr2[index])){
-    return true;
-  } else {
-  
+const eqArrays = require("./eqArrays");
+
+
+const eqObjects = function(obj1, obj2) {
+
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   }
-}
-const eqObjects = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let value = 0; value < array1.length; value += 1) {
-    if ((Array.isArray(array1[value]) || Array.isArray(array2[value])) && eqArrays(array1[value], array2[value]) === false) {
-      return false;
-    } else if (Array.isArray(array1[value]) || Array.isArray(array2[value])) {
-      eqArrays(array1[value], array2[value]);
-    } else if (array1[value] !== array2[value]) {
+
+  for (let key in obj1) {
+    if (Array.isArray(obj1[key]) &&  Array.isArray(obj2[key]) && obj1[key].length == obj2[key].length) { 
+      
+      if (eqArrays(obj1[key], obj2[key]) === false) {
+        return false;
+      }
+
+    } else if (obj1[key] !== obj2[key]) {
       return false;
     }
   }
   return true;
+  
+}
+
+
+// Testing
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 
 
 
-// const eqObjects = function(obj1, obj2) {
+// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
 
-//   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-//     return false;
-//   }
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
 
-//   for (let key in obj1) {
-//     if (Array.isArray(obj1[key]) &&  Array.isArray(obj2[key])) { 
-      
-//       if (eqArrays(obj1[key], obj2[key]) === false) {
-//         return false;
-//       }
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
 
-//     } else if (obj1[key] !== obj2[key]) {
-//       return false;
-//     }
-//   }
-//   return true;
-  
-// }
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
 
-
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-
-eqObjects(ab, ba);
-
-// const abc = { a: "1", b: "2", c: "3" };
-
-// assertEqual(eqObjects(ab, abc), false);
-
-// const cd = { c: "1", d: ["2", 3] };eqO
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false);
+module.exports = eqObjects;
