6e768ccfcf6d4713887c11fa2f2ce406416be1ee
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 16:52:18 -0400

    mocha version of test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,11 @@
+const assert = require('chai').assert;
+const tail = require("../tail");
 
-const tail = require("../tail") //two dots because the file is not in this folder
-const assertEqual = require("../assertEqual")
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const word = ["Lighthouse", "Labs"];
-tail(word); // no need to capture the return value since we are not checking it
-assertEqual(word.length, 3); // original array should still have 3 elements!
+describe('#tail', () =>{
+  it( "should return ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs'] " , () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']),['Lighthouse', 'Labs'] )
+  });
+  it("should return [] for array of length one such as [9]", () => {
+    assert.deepEqual(tail([5]), [])
+  })
+})
