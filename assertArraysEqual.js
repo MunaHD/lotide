83b6be1bef3d4055401ee5eb776275b4ec6ca461
Muna Dahir <56461233+MunaHD@users.noreply.github.com>
2021-11-01 15:31:09 -0400

    added JSON.stringify to print the objects

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,11 +6,13 @@ const assertArraysEqual = function (arr1, arr2){
   // uses the eqArray function to see if the arrays are equal
   if (eqArrays(arr1, arr2)) {
     //if the test passes
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
   } else{
     //if the test fails
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`)
+    console.log(`❌❌❌ Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`)
   }
 }
+
+
 
 module.exports = assertArraysEqual;
