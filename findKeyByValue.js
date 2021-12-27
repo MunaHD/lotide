be07f9af23d51f46d71e9a4941a10afcaf85a643
Muna Dahir <muna.dahir@hotmail.com>
2021-12-26 23:13:38 -0500

    remove assertEqual

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-// FUNCTION IMPLEMENTATION
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
 
 const findKeyByValue = function(object, value) {
   // loop through the keys and check for the value
