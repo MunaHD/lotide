04028aa71df57cc94422f10fb7d35996bad38e15
Muna Dahir <muna.dahir@hotmail.com>
2021-12-26 23:31:59 -0500

    lint code

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,18 @@
-
-
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i in arr1) {
-  if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-    if (!eqArrays(arr1[i], arr2[i])) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
-  } else if (arr1[i] !== arr2[i]) {
-    return false
   }
-}
-return true;
-}
+  return true;
+};
 
 module.exports = eqArrays;
 
