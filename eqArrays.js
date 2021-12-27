bb8651e7134a0fbea31f169d5b530fcd40a1e0b4
Muna Dahir <muna.dahir@hotmail.com>
2021-12-26 23:34:07 -0500

    add comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,17 @@
 const eqArrays = function(arr1, arr2) {
+  // if the lengths of the arrays don't match return false
   if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i in arr1) {
+    //check if  there are nested arrays
     if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // recursion to test the nested arrays
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
     } else if (arr1[i] !== arr2[i]) {
+      // return false if the elements don't match
       return false;
     }
   }
