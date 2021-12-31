aa29d2e07be82eda004af2401a709b36e6eb9f20
Muna Dahir <muna.dahir@hotmail.com>
2021-12-30 21:55:26 -0500

    lint code

--- takeUntil.js
+++ takeUntil.js
@@ -1,18 +1,14 @@
-const takeUntil = function(array, callback) {
+const takeUntil = function (array, callback) {
   // make array to hold new values
   let newArr = [];
-  // loop over the array elements
   for (let item of array) {
-    // as long as the condition in the function is false
-    //push the elements into the new array
+    // as long as the condition in the function is false push the elements into the new array
     if (!callback(item)) {
       newArr.push(item);
-      //once the condition is true then we return what 
-      // we added so far to the array so far
+      //once the condition is true then we return what we added so far to the array
     } else {
       return newArr;
-     
     }
   }
-}
+};
 module.exports = takeUntil;
