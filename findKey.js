a387137b74d95ad649fec43040657803587b2b2f
Muna Dahir <muna.dahir@hotmail.com>
2021-12-30 21:34:36 -0500

    simplify code and remove comments

--- findKey.js
+++ findKey.js
@@ -1,21 +1,9 @@
 const findKey = function(object, callback) {
 
-  // the function findKey which takes in an object and a callback.
-  // It should scan the object
-  for (let key in object) {
-    let subObj = object[key];
-    //return the first key for which
-    // the callback returns a truthy value. If no key is found,
-    // then it should return undefined.
-    subObj.find(callback(subObj))
-    // if (callback(subObj)) {
-    //   return key;
-    //}
-  }
+  //simplify code to use Object.keys
+  return Object.keys(object).find(key => callback(object[key]));
 };
 
 
+module.exports = findKey;
 
-module.exports = findkey;
-//personal test 
-//const findKey = (obj, callback) => Object.keys(obj).find(key => cb(obj[key]));
